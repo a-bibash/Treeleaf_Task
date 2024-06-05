c3c30f722633c8c173105b4a43507eaa4d8847f7
Bibash <workinprogress.bibash@gmail.com>
2024-06-05 15:24:09 +0545

    Merge templates branch into main

--- computerhub/computer/views.py
+++ computerhub/computer/views.py
@@ -37,12 +37,36 @@ def create(request):
     return render(request , 'create.html', context)
 
 
-def update(request):
-    return render(request , 'update.html')
+
+def update(request,id):
+    computer_up= Computer.objects.get(id=id)
+    computer = ComputerSpecification.objects.all()
+    context = {'computer':computer}
+    
+    if request.method =='POST':
+        data = request.POST
+        computer_code = data.get("computer_code")
+        computer = data.get("computer")
+        quantity = int(data.get("quantity"))
+        unit_rate =int(data.get("unit_rate"))
+                
+        computer_name = ComputerSpecification.objects.get(brand=computer)
+        
+        
+        computer_up.computer_code = computer_code
+        computer_up.computer = computer_name
+        computer_up.quantity = quantity
+        computer_up.unit_rate = unit_rate
+        
+        computer_up.save()
+            
+        return redirect('/')
+    return render(request , 'update.html',context)
 
 
-def delete(request):
+def delete(request,id):
+    computer_del= Computer.objects.get(id=id)
+    computer_del.delete()
     return redirect('/')
-
 
 
